bea901e3c26083c1cba71e440d4bc27c7987603c
Alexander Markov <apsheronets@gmail.com>
2014-11-30 10:01:12 +0300

    zoom calculations

--- main.ml
+++ main.ml
@@ -50,6 +50,63 @@ let print_gpx =
       (List.iter
         (fun (lat, lon) -> Printf.printf "(%f, %f)\n" lat lon)))
 
+let tile_width = 256
+let tile_height = 256
+
+let calc_border init f (gpx:gpx) : float =
+  let r = ref init in
+  List.iter
+    (List.iter
+      (List.iter
+        (fun latlon -> f latlon r)))
+    gpx;
+  if !r = init
+  then assert false
+  else !r
+
+let min_lat =
+  calc_border ( 666.) (fun (lat, _) r -> if !r > lat then r := lat)
+
+let min_lon =
+  calc_border ( 666.) (fun (_, lon) r -> if !r > lon then r := lon)
+
+let max_lat =
+  calc_border (-666.) (fun (lat, _) r -> if !r < lat then r := lat)
+
+let max_lon =
+  calc_border (-666.) (fun (_, lon) r -> if !r < lon then r := lon)
+
+let calc_zoom lat_min lat_max lon_min lon_max width height =
+  let zoom_to_fit_width = int_of_float (floor (Math.log2(float_of_int (360 * width) /. (float_of_int tile_width *. (lon_max -. lon_min))))) in
+  Printf.printf "Calculated maximum zoom to fit width %d\n" zoom_to_fit_width;
+  let zoom_to_fit_height = int_of_float (floor (Math.log2 (float_of_int height /. (float_of_int tile_height *. (log (tan (lat_max *. (Constant.pi /. 180.0)) +. (1.0 /. cos (lat_max *. (Constant.pi /. 180.0)))) -. log (tan (lat_min *. (Constant.pi /. 180.0)) +. (1.0 /. cos(lat_min *. (Constant.pi /. 180.0))))) /. Constant.pi))) +. 1.) in
+  Printf.printf "Calculated maximum zoom to fit height %d\n" zoom_to_fit_height;
+  let zoom =
+    if zoom_to_fit_width < zoom_to_fit_height
+    then zoom_to_fit_width
+    else zoom_to_fit_height in
+  Printf.printf "Calculated minimum zoom %d\n" zoom;
+  if zoom > 18
+  then (* in case we got one meter GPX *)
+    (Printf.printf "Zoom is too high, choose 18\n";
+    18)
+  else if zoom < 0
+  then
+    (Printf.printf "Zoom is too low, choose 0";
+    0)
+  else
+    zoom
+
 let () =
   let gpx = gpx_of_channel stdin in
-  print_gpx gpx
+  print_gpx gpx;
+  ignore (
+    calc_zoom
+      (min_lat gpx)
+      (max_lat gpx)
+      (min_lon gpx)
+      (max_lon gpx)
+      300
+      300
+  )
+
